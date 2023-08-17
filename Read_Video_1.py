5b9010d8d1640781d52ad7aa48119e08fbe59277
AviHackerPro <110382912+AviHackerPro@users.noreply.github.com>
2023-08-17 18:15:23 +1000

    Add files via upload

---
+++ Read_Video_1.py
@@ -0,0 +1,21 @@
+import cv2
+
+vid = cv2.VideoCapture("AnthonyShkraba.mp4")
+if(vid.isOpened() == False):
+    print("Unable to read the fid")
+height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+fps = int(vid.get(cv2.CAP_PROP_FPS))
+print(height, width, fps)
+
+out = cv2.VideoWriter("MyVideo1.mp4", cv2.VideoWriter_fourcc(*'DIVX'), 30, (width, height))
+
+while(True):
+    ret, frame = vid.read()
+    cv2.imshow("Webcam", frame)
+    out.write(frame)
+    if(cv2.waitKey(1) == 32):
+        break
+vid.release()
+cv2.destroyAllWindows()
+out.release()
